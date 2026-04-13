4d9555d41c0b1d10edac32f17d1ef19ca5a6e7bc
arjun kumar <kumararjun5230@gmail.com>
2026-04-13 22:36:14 +0530

    Update backend URL to new Vercel domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://book-my-show-backend-kohl.vercel.app',
+  baseURL: 'https://book-my-show-backend-bejg.vercel.app',
 });
 
 // Attach JWT token to every request if available
